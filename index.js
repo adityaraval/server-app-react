14026448f95a4657902eb92dda7f7563b19488a6
Aditya Raval <adi7017@gmail.com>
2017-11-28 19:14:31 +0530

    added passport

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').GoogleStrategy;
 const app = express();
 
 app.get('/',(req,res)=>{
     res.send({hi:'there'});
 });
+
+//console.developers.google.com
+passport.use(new GoogleStrategy({}))
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT);
